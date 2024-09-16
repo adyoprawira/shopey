6a7cb719ea75067e70e579a0e1c6004eebf4a506
adyoprawira <adyoprawira@gmail.com>
2024-09-17 02:23:12 +0700

    Finish assignment 3

--- shopey/settings.py
+++ shopey/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'shopey.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
