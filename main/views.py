99b8504a5153a989fc3b18bd709ab91cb6169795
adyoprawira <adyoprawira@gmail.com>
2024-10-09 23:43:59 +0700

    Finished Assignment 6

--- main/views.py
+++ main/views.py
@@ -9,16 +9,17 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
-    product_entries = Product.objects.filter(user=request.user)
 
     context = {
         'app_name' : 'Shopey',
         'name' : request.user.username,
         'class' : 'KKI',
-        'product_entries': product_entries,
         'last_login': request.COOKIES.get('last_login', 'Not set'),
     }
     
@@ -37,11 +38,11 @@ def create_product(request):
     return render(request, "create_product.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
@@ -74,7 +75,8 @@ def login_user(request):
             response = HttpResponseRedirect(reverse("main:show_main"))
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
-
+        else:
+            messages.error(request, "Invalid username or password. Please try again.")
     else:
         form = AuthenticationForm(request)
     context = {'form': form}
@@ -108,3 +110,23 @@ def delete_product(request, id):
     product.delete()
     # Return to home page
     return HttpResponseRedirect(reverse('main:show_main'))
+
+@csrf_exempt
+@require_POST
+def add_product_entry_ajax(request):
+    name = strip_tags(request.POST.get("name"))
+    description = strip_tags(request.POST.get("description"))
+    price = strip_tags(request.POST.get("price"))
+    category = strip_tags(request.POST.get("category"))
+    user = request.user
+
+    new_product = Product(
+        name=name,
+        description=description,
+        price=price,
+        user=user,
+        category=category,
+    )
+    new_product.save()
+
+    return HttpResponse(b"CREATED", status=201)
